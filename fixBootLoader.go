78775334a1fe9d6ec2d0776dbb0c1d4791cc93b1
Chanadu <chandu.peddada@gmail.com>
2024-07-11 20:52:39 -0500

    Replace Titles

--- fixBootLoader.go
+++ fixBootLoader.go
@@ -5,6 +5,7 @@ import (
 	"log"
 	"os"
 	"os/user"
+	"strings"
 )
 
 func isRoot() bool {
@@ -13,6 +14,29 @@ func isRoot() bool {
 		log.Fatalf("[isRoot] Unable to get current user: %s", err)
 	}
 	return currentUser.Username == "root"
+}
+
+func getEntryType(fileName string) (entryType string, isFallback bool, err error) {
+	isFallback = false
+	entryType = ""
+	lowerFileName := strings.ToLower(fileName)
+	if strings.Contains(lowerFileName, "arch") {
+		entryType = "arch"
+	}
+	if strings.Contains(lowerFileName, "windows") {
+		entryType = "windows"
+	}
+	if strings.Contains(lowerFileName, "lts") {
+		entryType = "lts"
+	}
+	if strings.Contains(lowerFileName, "fallback") {
+		isFallback = true
+	}
+	if entryType == "" {
+		err = fmt.Errorf("Unknown entry type")
+		return "", false, err
+	}
+	return entryType, isFallback, nil
 }
 
 func main() {
@@ -32,18 +56,45 @@ func main() {
 
 	for _, file := range files {
 		fmt.Println(file.Name(), file.IsDir())
-		// if file.IsDir() {
-		// 	continue
-		// }
+		if file.IsDir() {
+			continue
+		}
 
-		// fileName := file.Name()
-		// if fileName == "arch.conf" {
-		// 	continue
-		// }
+		fileName := file.Name()
 
-		// err := os.Remove("/efi/loader/entries/" + fileName)
-		// if err != nil {
-		// 	os.Exit(1)
-		// }
+		fileData, err := os.ReadFile("/efi/loader/entries/" + fileName)
+		if err != nil {
+			log.Fatalf("Unable to read file: %s", err)
+			os.Exit(1)
+		}
+		// fmt.Println()
+		// fmt.Println(string(fileData))
+		// fmt.Println()
+
+		fileLines := strings.Split(string(fileData), "\n")
+		entryType, isFallback, err := getEntryType(fileName)
+		if err != nil {
+			log.Fatalf("Unable to determine entry type: %s", err)
+			os.Exit(1)
+		}
+
+		entryTypeToTitle := map[string]string{
+			"arch":    "EndeavourOS-Arch",
+			"windows": "Windows",
+			"lts":     "EndeavourOS-LTS",
+		}
+
+		for _, line := range fileLines {
+			if strings.Contains(line, "title") {
+				fallbackText := ""
+				if isFallback {
+					fallbackText = "-Fallback"
+				}
+
+				fmt.Println(line)
+				fmt.Println("Replace with :", entryTypeToTitle[entryType]+fallbackText)
+			}
+		}
 	}
+
 }
